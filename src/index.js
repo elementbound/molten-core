6a56898951c8a1d1e6ccea45efc6cfc454b49f3e
Tamas Galffy <ezittgtx@gmail.com>
2019-01-09 15:56:46 +0100

    Webpack and threejs setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('Hello world!')
+const three = require('three')
+
+const setupCanvas = (root) => {
+    const canvas = document.createElement('canvas')
+    root.appendChild(canvas)
+}
+
+window.onload = () => 
+    setupCanvas(document.querySelector('body'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path')
 
+const DIST = path.resolve(__dirname, 'dist')
+
 module.exports = {
+    // mode: 'development',
     entry: './src/index.js',
+
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: DIST
+    },
+
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
+        path: DIST
     }
 }
